637378c8fba7c8cb0a94731125ca037dca588e1a
Brian Lego <legobw@gmail.com>
2020-06-26 14:04:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,11 +2,8 @@ require 'pry'
 
 def reverse_each_word (sentence)
   string_sentence = sentence.split(" ")
-  reverse_string_sentence = ""
   string_sentence.collect do |word|
-    
     word.reverse!
-    end
-
+  end
 string_sentence.join(" ")
 end 
