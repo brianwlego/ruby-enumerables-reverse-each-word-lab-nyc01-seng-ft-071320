1e9bbeaffdd5052a3f51ee1b3a6ffbbe7570a811
Brian Lego <legobw@gmail.com>
2020-06-26 13:55:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word (sentence)
   string_sentence = sentence.split(" ")
   binding.pry
   string_sentence.collect do |word|
-    word.reverse_each {|drow| p drow }
+    word.reverse! }
     end
   
   
