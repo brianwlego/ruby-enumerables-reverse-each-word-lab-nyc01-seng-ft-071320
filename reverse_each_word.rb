9957737a238c517e0e0afa2f3ddb2411a49b2bd2
Brian Lego <legobw@gmail.com>
2020-06-26 13:53:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,9 @@ require 'pry'
 def reverse_each_word (sentence)
   string_sentence = sentence.split(" ")
   binding.pry
-  string_sentence.collect {|word| word.reverse_each_word}
+  string_sentence.collect do |word|
+    word.reverse_each {|drow| p drow }
+    end
   
   
   binding.pry
